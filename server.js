8887eadc5fe9bae355a02ea303ccdbc8405c3438
박상원 <sangwon@sungwon-iMac.local>
2020-12-25 22:28:13 +0900

    feature: User GQL 추가
    * user schema 추가

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const { ApolloServer, gql } = require('apollo-server');
 const db = require('./config/database');
 
-const typeDefs = require('./lib/types/schema');
+const typeDefs = require('./lib/typeDefs/schema');
 const resolvers = require('./lib/resolvers/schema');
  
 const server = new ApolloServer({ 
     typeDefs, 
-    resolvers 
+    resolvers,
+    context: ({req, res}) => {
+        console.log('req - ', req.body);
+    }
 });
 
 
