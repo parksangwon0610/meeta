a6909c9de7260bf9a4f6fdde51400b09f0fb7b7a
박상원 <sangwon@sungwon-iMac.local>
2020-12-27 02:03:52 +0900

    fix: Database ts 변경

---
+++ config/database.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = 'mongodb://localhost:27017/meeta';
+export const connect = () => {
+    mongoose.connect(
+            MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+    
+    const database = mongoose.connection;
+    database.once("open",() =>{
+        console.log('connected database');
+    })
+};
