52ebc219a710be1f7cf8b05cad78eab7a42da346
박상원 <sangwon@sungwon-iMac.local>
2020-12-26 21:11:30 +0900

    fix: Export database connection

--- config/database.js
+++ config/database.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
 
 const MONGO_URI = 'mongodb://localhost:27017/meeta';
-mongoose.connect(
-        MONGO_URI,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    );
+const connect = () => {
+    mongoose.connect(
+            MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+    
+    const db = mongoose.connection;
+    db.then(db => {
+            console.log('connected database to ' + MONGO_URI);
+            return db;
+        }).catch(err => {
+            if(err.message.code === 'ETIMEDOUT') {
+                console.log('Attempting to re-establish database connection') 
+                mongoose.connect(MONGO_URI);
+            } else {
+                console.log('Error while sattempting to connect to database: ' + err);
+            }
+        });
+}
 
-const db = mongoose.connection;
-db.then(db => {
-        console.log('mongo data connected');
-        return db;
-    }).catch(err => {
-        if(err.message.code === 'ETIMEDOUT') {
-            console.log('Attempting to re-establish database connection') 
-            mongoose.connect(MONGO_URI);
-        } else {
-            console.log('Error while sattempting to connect to database: ' + err);
-        }
-    });
+module.exports = { connect };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
 const { ApolloServer, gql } = require('apollo-server');
-const db = require('./config/database');
+const database = require('./config/database');
 
 const typeDefs = require('./lib/typeDefs/schema');
 const resolvers = require('./lib/resolvers/schema');
  
+database.connect();
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
